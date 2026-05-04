8ec04c5cbeb1d9854231878e6ad2b11b03301414
mrbor <thisonlystudy@gmail.com>
2026-05-04 20:47:36 +0500

    fix: resolve startup error and clean up user/auth issues
    - Remove duplicate `profile` field in `User`
    - Fix 401 response message output
    - Drop redundant `idx_token` JPA index declaration to avoid "relation already exists" on startup

--- src/main/java/com/gamevault/db/model/User.java
+++ src/main/java/com/gamevault/db/model/User.java
@@ -43,9 +43,6 @@ public class User extends BaseEntity implements UserDetails {
     @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private UserProfile profile;
 
-    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-    private UserProfile profile;
-
     public User(String username, String password, List<String> roles) {
         this.username = username;
         this.password = password;
